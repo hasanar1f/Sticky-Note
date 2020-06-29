fc2fa6ddd35c404c5b9b6814fd3a7a82e0203235
GitFazz <efaz701815@gmail.com>
2020-06-29 06:46:56 +0600

    hudai

---
+++ app.js
@@ -0,0 +1,79 @@
+const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
+
+
+//create element and rander cafe data
+
+function renderCafe(doc) {
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let city = document.createElement('span');
+    let cross = document.createElement('div');
+
+    li.setAttribute('data-id',doc.id);
+    name.textContent = doc.data().name;
+    city.textContent = doc.data().city;
+    cross.textContent = 'done';
+
+    li.appendChild(name);
+    li.appendChild(city);
+    li.appendChild(cross);
+
+
+    //finally,
+
+    cafeList.appendChild(li);
+
+    //listen to the cross button
+
+    cross.addEventListener('click',(e)=>{
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('cafes').doc(id).delete();
+    })
+ }
+
+ //getting data
+// db.collection('cafes').orderBy('city').get().then((snapshot)=>{
+//     snapshot.docs.forEach(doc =>{
+//         renderCafe(doc);
+//     })
+// });
+
+//setting data
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    });
+
+    form.name.value = '';
+    form.city.value = '';
+
+})
+
+
+
+// Real time listener
+
+db.collection('cafes').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    console.log(changes);
+    changes.forEach(change => {
+        console.log(change.doc.data());
+        if(change.type == 'added') {
+            renderCafe(change.doc);
+        } else if (change.type == 'removed')
+        {
+            let li = cafeList.querySelector('[data-id='+change.doc.id+']');
+            cafeList.removeChild(li);
+        }
+    })
+})
+
+
+
+
+
